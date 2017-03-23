a308f8bb20171d788a4a1c22b833c1df050d1eaf
tomjfarrell <tomjfarrell@gmail.com>
2017-03-23 18:47:52 +1100

    Adding file reading from front in addition to existing back

--- Examples/File Reader tool/Attempt03/main.go
+++ Examples/File Reader tool/Attempt03/main.go
@@ -8,8 +8,8 @@ import (
 )
 
 var file = flag.String("file", "/var/tmp/text", "source file")
-var alines =flag.Int("alines", 10, "lines to read, starting from beginning")
-var zlines = flag.Int("zlines", 10, "lines to read, starting from end")
+var alines =flag.Int("alines", 0, "lines to read, starting from beginning")
+var zlines = flag.Int("zlines", 0, "lines to read, starting from end")
 
 func check(e error) {
 	if e != nil {
@@ -23,20 +23,40 @@ func filereader(file string) []byte {
 	return dat
 }
 
+func printrequest(file string, l,a,z int) {
+	fmt.Printf("file: %s, length of file %d\n", file, l)
+	if (a == 0) && (z == 0) {
+		fmt.Println("Need --alines and/or --zlines flag(s)")
+	}
+}
+
+
 func main() {
 
 	flag.Parse()
 
-	fmt.Printf("file: %s, counting back %d lines\n",*file,*lines)
-
 	data := filereader(*file)
 	contents := strings.Split(string(data), "\n")
-	fmt.Printf("lenth of file: %d\n",len(contents)-1)
-	if len(contents) < 10 {
-		fmt.Println(string(data))
+
+	printrequest(*file,len(contents),*alines,*zlines)
+
+	if (len(contents) < *alines) && (len(contents) < *zlines) {
+		fmt.Println("Not enough contents to fulfill request, printing entire file.")
+		for i := 0; i < len(contents)-1; i++ {
+			fmt.Println(i+1, ":", contents[i])
+		}
 	} else {
-		for i := len(contents)-1-int(*lines); i < len(contents)-1; i++ {
-			fmt.Println(i+1,":",contents[i])
+		if *alines > 0 {
+			fmt.Printf("reading first %d lines:\n", *alines)
+			for i := 0; i < *alines; i++ {
+				fmt.Println(i+1, ":", contents[i])
+			}
+		}
+		if *zlines > 0 {
+			fmt.Printf("reading last %d lines:\n", *alines)
+			for i := len(contents)-1-int(*zlines); i < len(contents)-1; i++ {
+				fmt.Println(i+1,":",contents[i])
+			}
 		}
 	}
 }
