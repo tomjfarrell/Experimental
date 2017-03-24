7267eb51d44265a185dcca98988e3d5d4dc2cf96
tomfarrell <tomjfarrell@gmail.com>
2017-03-24 16:11:39 +1100

    Slowly trying to migrate from reading entire file to chunks, playing with os package for this.

--- Examples/File Reader tool/Attempt05/main.go
+++ Examples/File Reader tool/Attempt05/main.go
@@ -18,30 +18,46 @@ func check(e error) {
 	}
 }
 
-func filereader(file string) []byte {
-	dat, err := ioutil.ReadFile(file)
-	check(err)
-	return dat
-}
-func filereader2(file string, size int64) {
+//func filereader(file string) []byte {
+//	dat, err := ioutil.ReadFile(file)
+//	check(err)
+//	return dat
+//}
+
+func filereader(file string, size int64) {
   f, err := os.Open(file)
 	check(err)
-  chunksize := 500
-	data := make([]byte, chunksize)
-	for i := int(size); i > 0; i-=chunksize {
-		if i >= 500 {
-			fmt.Printf("***reading %v bytes, starting at offset %v***\n",len(data),i-chunksize)
-			count, err := f.ReadAt(data,int64(i-chunksize))
+  chunk_size := 500
+	data := make([]byte, chunk_size)
+	for i := int(size); i > 0; i-= chunk_size {
+		if i >= chunk_size {
+			fmt.Printf("***reading %v bytes, starting at offset %v***\n",len(data),i- chunk_size)
+			count, err := f.ReadAt(data,int64(i- chunk_size))
 			check(err)
-			fmt.Println(string(data[:count]))
+			contents := strings.Split(string(data[:count]), "\n")
+			length := len(contents)-1
+			for i := 0; i < length; i++ {
+				fmt.Println(i + 1, ":", contents[i])
+			}
+			fmt.Printf("read %d bytes: %q\n", count, data[:count])
 		} else {
 			fmt.Printf("***less than %v bytes left, printing remaining %v from beginning***\n",len(data),i)
 			remainder := make([]byte,i)
 			count, err := f.Read(remainder)
 			check(err)
-			fmt.Println(string(remainder[:count]))
+			contents := strings.Split(string(remainder[:count]), "\n")
+			length := len(contents)-1
+			for i := 0; i < length; i++ {
+				fmt.Println(i + 1, ":", contents[i])
+			}
+			fmt.Printf("read %d bytes: %q\n", count, remainder[:count])
+
 		}
 	}
+}
+
+func linefinder(data string, lines int) {
+  //will find lines received from filereader
 }
 
 func printrequest(file string, a,z int) int64 {
@@ -60,30 +76,30 @@ func main() {
 
 	flag.Parse()
 
-	data := filereader(*file)
-	contents := strings.Split(string(data), "\n")
-	length := len(contents)-1
+	//data := filereader(*file)
+	//contents := strings.Split(string(data), "\n")
+	//length := len(contents)-1
 	size := printrequest(*file,*alines,*zlines)
-  filereader2(*file,size)
+  filereader(*file,size)
 
-	if (length < (*alines + *zlines)) {
-		fmt.Println("Not enough contents to fulfill request, printing entire file.")
-		fmt.Printf("There will be an overlap of %d line(s)\n",(*alines+*zlines-length))
-		for i := 0; i < length; i++ {
-			fmt.Println(i+1, ":", contents[i])
-		}
-	} else {
-		if *alines > 0 {
-			fmt.Printf("reading first %d lines:\n", *alines)
-			for i := 0; i < *alines; i++ {
-				fmt.Println(i+1, ":", contents[i])
-			}
-		}
-		if *zlines > 0 {
-			fmt.Printf("reading last %d lines:\n", *zlines)
-			for i := length-int(*zlines); i < length; i++ {
-				fmt.Println(i+1,":",contents[i])
-			}
-		}
-	}
+	//if (length < (*alines + *zlines)) {
+	//	fmt.Println("Not enough contents to fulfill request, printing entire file.")
+	//	fmt.Printf("There will be an overlap of %d line(s)\n",(*alines+*zlines-length))
+	//	for i := 0; i < length; i++ {
+	//		fmt.Println(i+1, ":", contents[i])
+	//	}
+	//} else {
+	//	if *alines > 0 {
+	//		fmt.Printf("reading first %d lines:\n", *alines)
+	//		for i := 0; i < *alines; i++ {
+	//			fmt.Println(i+1, ":", contents[i])
+	//		}
+	//	}
+	//	if *zlines > 0 {
+	//		fmt.Printf("reading last %d lines:\n", *zlines)
+	//		for i := length-int(*zlines); i < length; i++ {
+	//			fmt.Println(i+1,":",contents[i])
+	//		}
+	//	}
+	//}
 }
