8aab06f60a2b41aa815149cbb243bdbefa26a8b8
tomfarrell <tomjfarrell@gmail.com>
2017-04-11 16:24:18 +1000

    yet another attempt to get readat working

--- Examples/File Reader tool/Attempt07/main.go
+++ Examples/File Reader tool/Attempt07/main.go
@@ -25,17 +25,30 @@ func file_reader(file string, size int64, lines int) []string {
 	check(err)
 	chunk_size := 500
 	data := make([]byte, chunk_size)
+
+	lastlinecheck := make([]byte, 1)
+	count, err := f.ReadAt(lastlinecheck, int64(size - 1))
+	check(err)
+	if string(lastlinecheck[:count]) == "\n" {
+		size--
+	}
+
 	for i := int(size); i > 0 && len(output) < lines; i -= chunk_size {
 		if i >= chunk_size {
+			fmt.Printf("Starting read at: %v\n\n", i - chunk_size)
 			count, err := f.ReadAt(data, int64(i - chunk_size))
 			check(err)
 			leftover = string(data[:count]) + leftover
+			fmt.Printf("Read: %v\n\n", leftover)
 			if strings.Contains(leftover, "\n") {
+				fmt.Println("(f>c) new lines found\n")
 				array := line_finder(leftover)
-				leftover = array[0] + leftover
-				output = append(array[1:], output)
+				leftover = array[0]
+				fmt.Printf("adding to leftover: %v\n\n", array[0])
+				output = append(array[1:], output...)
+				fmt.Printf("output array so far (length: %v): %v\n\n", len(output), output)
 			} else {
-				output = append([]string{leftover}, output)
+				fmt.Println("(f>c) no new lines found\n")
 			}
 		} else {
 			remainder := make([]byte, i)
@@ -43,11 +56,13 @@ func file_reader(file string, size int64, lines int) []string {
 			check(err)
 			leftover = string(remainder[:count]) + leftover
 			if strings.Contains(leftover, "\n") {
+				fmt.Println("(c>f) new lines found\n")
 				array := line_finder(leftover)
-				leftover = array[0] + leftover
-				output = append(array[1:], output)
+				leftover = array[0]
+				output = append(array, output...)
 				} else {
-				output = append([]string{leftover}, output)
+				fmt.Println("(c>f) no new lines found\n")
+				output = append([]string{leftover}, output...)
 			}
 		}
 		}
@@ -55,8 +70,11 @@ func file_reader(file string, size int64, lines int) []string {
 }
 
 func line_finder(text string) []string {
-  var arrayout []string
-	arrayout = strings.Split(text, "\n") //split at \n
+	arrayout := strings.Split(text, "\n")
+	fmt.Println("line_finder found these lines:")
+	for i := 0; i < len(arrayout); i++{
+		fmt.Printf("arrayout[%v]: %v\n\n", i, arrayout[i])
+	}
 	return arrayout
 }
 
@@ -65,10 +83,10 @@ func printrequest(file string, a,z int) int64 {
 	check(err)
 	fmt.Printf("Filename: '%s'|Size: %d bytes|Modified: %v\n", stat.Name(),stat.Size(),stat.ModTime())
 	if a != 0 {
-		fmt.Printf("Pulling first %v lines.\n", a)
+		fmt.Printf("Pulling first %v lines...\n", a)
 	}
 	if z != 0 {
-		fmt.Printf("Pulling last %v lines.\n", z)
+		fmt.Printf("Pulling last %v lines...\n", z)
 	}
 	return stat.Size()
 }
@@ -78,5 +96,8 @@ func main() {
 	flag.Parse()
 
 	size := printrequest(*file, *alines, *zlines)
-	file_reader(*file, size, *zlines)
+	output := file_reader(*file, size, *zlines)
+	for i := 0; i < len(output) && i+1 <= *zlines; i++ {
+		fmt.Printf("Line #%v: %v\n\n", i+1, output[i])
+	}
 }
