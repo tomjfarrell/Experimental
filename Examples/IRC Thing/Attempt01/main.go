554034f5c894919e81436afe9ba6ec93168e368d
tomjfarrell <tomjfarrell@gmail.com>
2017-04-20 14:21:19 +1000

    Server runs now, time to start on client

--- Examples/IRC Thing/Attempt01/main.go
+++ Examples/IRC Thing/Attempt01/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"os"
 	"flag"
@@ -9,6 +8,7 @@ import (
 	"io"
 	"io/ioutil"
 	"container/list"
+	"bytes"
 )
 
 const (
@@ -51,6 +51,96 @@ errorHandle io.Writer) {
 }
 
 
+type ClientChat struct {
+	Name string;        // name of user
+	IN chan string;     // input channel for to send to user
+	OUT chan string;    // input channel from user to all
+	Con net.Conn;      // connection of client
+	Quit chan bool;     // quit channel for all goroutines
+	ListChain *list.List;    // reference to list
+}
+
+// close the connection and send quit to sender
+func (c *ClientChat) Close() {
+	c.Quit<-true
+	c.Con.Close()
+	c.deleteFromList()
+}
+
+// compare two clients: name and network connection
+func (c *ClientChat) Equal(cl *ClientChat) bool {
+	if bytes.Equal([]byte(c.Name), []byte(cl.Name)) {
+		if c.Con == cl.Con {
+			return true
+		}
+	}
+	return false
+}
+
+// delete the client from list
+func (c *ClientChat) deleteFromList() {
+	for e := c.ListChain.Front(); e != nil; e = e.Next() {
+		client := e.Value.(ClientChat)
+		if c.Equal(&client) {
+			Info.Println("deleteFromList(): ", c.Name)
+			c.ListChain.Remove(e)
+		}
+	}
+}
+
+// read from connection and return true if ok
+func (c *ClientChat) Read(buf []byte) bool{
+	nr, err := c.Con.Read(buf)
+	if err!=nil {
+		c.Close()
+		return false;
+	}
+	Info.Println("Read():  ", nr, " bytes")
+	return true
+}
+
+// clientreceiver wait for an input from network, after geting data it send to
+// handlingINOUT via a channel.
+func clientreceiver(client *ClientChat) {
+	buf := make([]byte, 2048)
+
+	Info.Println("clientreceiver(): start for: ", client.Name)
+	for client.Read(buf) {
+
+		if bytes.Equal(buf, []byte("/quit")) {
+			client.Close()
+			break;
+		}
+		Info.Println("clientreceiver(): received from ",client.Name, " (", string(buf), ")")
+		send := client.Name+"> "+string(buf)
+		client.OUT<- send
+		for i:=0; i<2048;i++ {
+			buf[i]=0x00
+		}
+	}
+
+	client.OUT <- client.Name+" has left chat"
+	Info.Println("clientreceiver(): stop for: ", client.Name)
+}
+
+// clientsender(): get the data from handlingINOUT via channel (or quit signal from
+// clientreceiver) and send it via network
+func clientsender(client *ClientChat) {
+	Info.Println("clientsender(): start for: ", client.Name)
+	for {
+		Info.Println("clientsender(): wait for input to send")
+		select {
+		case buf := <- client.IN:
+			Info.Println("clientsender(): send to \"", client.Name, "\": ", string(buf))
+			client.Con.Write([]byte(buf))
+		case <-client.Quit:
+			Info.Println("clientsender(): client want to quit")
+			client.Con.Close()
+			break;
+		}
+	}
+	Info.Println("clientsender(): stop for: ", client.Name)
+}
 
 // handlingINOUT(): handle inputs from client, and send it to all other client via channels.
 func handlingINOUT(IN <-chan string, lst *list.List) {
@@ -59,8 +149,8 @@ func handlingINOUT(IN <-chan string, lst *list.List) {
 		input := <-IN;  // input, get from client
 		// send to all client back
 		Info.Println("handlingINOUT(): handling input: ", input)
-		for value := range lst.Iter() {
-			client := value.(ClientChat)
+		for e := lst.Front(); e != nil; e = e.Next() {
+			client := e.Value.(ClientChat)
 			Info.Println("handlingINOUT(): send to client: ", client.Name)
 			client.IN<- input
 		}
@@ -69,7 +159,7 @@ func handlingINOUT(IN <-chan string, lst *list.List) {
 
 // clientHandling(): get the username and create the clientsturct
 // start the clientsender/receiver, add client to list.
-func clientHandling(con *net.Conn, ch chan string, lst *list.List) {
+func clientHandling(con net.Conn, ch chan string, lst *list.List) {
 	buf := make([]byte, 1024);
 	con.Read(buf);
 	name := string(buf);
@@ -96,16 +186,17 @@ func main() {
 	go handlingINOUT(in, clientlist)
 
 	// create the connection
-	netlisten, err := net.Listen("tcp", "127.0.0.1:9988")
+	hostport := CONN_HOST+":"+CONN_PORT
+	netlisten, err := net.Listen("tcp", hostport)
 	Error.Println(err, "main Listen")
 	defer netlisten.Close()
 
 	for {
 		// wait for clients
-		Info.Println("main(): wait for client ...");
-		conn, err := netlisten.Accept();
-		Error.Println(err, "main: Accept for client");
-		go clientHandling(&conn, in, clientlist);
+		Info.Println("main(): wait for client ...")
+		conn, err := netlisten.Accept()
+		Error.Println(err, "main: Accept for client")
+		go clientHandling(conn, in, clientlist)
 	}
 }
 
