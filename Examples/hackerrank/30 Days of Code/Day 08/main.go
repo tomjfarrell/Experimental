ac377af50f05ecfcffbd7b709d1773d6d16d4dd6
tomjfarrell <tomjfarrell@gmail.com>
2017-05-08 18:50:56 +1000

    Starting hackerrank exercises.

--- Examples/hackerrank/30 Days of Code/Day 08/main.go
+++ Examples/hackerrank/30 Days of Code/Day 08/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	var n int
-	fmt.Print("Input directory size(int): ")
 	fmt.Scan(&n)
 
 
@@ -15,22 +15,21 @@ func main() {
 	var phone int
 
 	for i := 0; i < n; i++ {
-		fmt.Printf("N = %v | i = %v\n", n, i)
-		fmt.Print("Input record(name phone_number): ")
 		fmt.Scan(&name, &phone)
-		fmt.Printf("Name received: %v | Phone received: %v\n", name, phone)
 		m[name] = phone
-		fmt.Println(m)
 	}
 
-  for {
-	  fmt.Scan(&name)
-	  _, ok := m[name]
-	  if !ok {
-		  fmt.Println("Not found")
-	  } else {
-		  fmt.Printf("%v=%v\n", name, phone)
-	  }
-  }
-
+	for {
+		read, _ := fmt.Scan(&name)
+		if read > 0 {
+			phone, ok := m[name]
+			if !ok {
+				fmt.Println("Not found")
+			} else {
+				fmt.Printf("%v=%v\n", name, phone)
+			}
+		} else {
+			os.Exit(1)
+		}
+	}
 }
