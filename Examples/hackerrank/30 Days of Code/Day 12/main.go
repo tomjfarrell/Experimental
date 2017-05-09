920b873e3fcac841c980edda9f22b1660fd4cb8e
tomjfarrell <tomjfarrell@gmail.com>
2017-05-09 18:39:56 +1000

    Starting hackerrank exercises.

--- Examples/hackerrank/30 Days of Code/Day 12/main.go
+++ Examples/hackerrank/30 Days of Code/Day 12/main.go
@@ -5,7 +5,6 @@ import (
 )
 
 func main() {
-	var m = make(map[byte]bool)
 	var n int
 	fmt.Scan(&n)
 	var s1 string
@@ -13,23 +12,39 @@ func main() {
 
 
 	for i := 0; i < n; i++ {
+		var m = make(map[byte]bool)
+		var z = "NO"
 		fmt.Scan(&s1)
 		fmt.Scan(&s2)
-    if len(s1) > len(s2) {
-	    for _,c := range s2 {
-		    m[c] = true
-		    for _,c := range s1 {
-			    if m[c] {
-				    fmt.Println("YES")
-				    break
-			    }
-		    }
-	    }
-    else {
+		if len(s1) > len(s2) {
+			fmt.Println("First word is larger, mapping second.")
+			for _, c := range s2 {
+				m[byte(c)] = true
+				fmt.Printf("Set m[%v] = true\n",c)
+				for _, c := range s1 {
+					if m[byte(c)] {
+						fmt.Printf("m[%v] found in map\n", c)
+						z = "YES"
+						break
+					} else {
+						fmt.Printf("m[%v] not found in map (%v).\n", c, m)
+					}
+				}
+			}
+		} else {
+			fmt.Println("Second word is larger, mapping first.")
 	    for _,c := range s1 {
-		    m[c] = true
+		    m[byte(c)] = true
+		    fmt.Printf("Set m[%v] = true\n",c)
+		    for _, c := range s2 {
+			    if m[byte(c)] {
+				    fmt.Printf("m[%v] found in map\n", c)
+				    z = "YES"
+				    break
+			    } else {
+				    fmt.Printf("m[%v] not found in map (%v).\n", c, m)
+			    }
 	    }
-
 		}
 		}
 		fmt.Println(z)
